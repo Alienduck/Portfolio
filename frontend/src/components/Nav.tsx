c944eb3951a620b6610c1cd142705c0e99a9ebfe
Owen Neuillyr <jacquonneries@gmail.com>
2026-02-25 10:21:25 +0100

    add courses page link

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -34,6 +34,7 @@ export default function Nav() {
         <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
         <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link></li>
         <li><Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>Projects</Link></li>
+        <li><Link to="/courses" className={location.pathname === '/courses' ? 'active' : ''}>Cours</Link></li>
       </ul>
     </nav>
   )
